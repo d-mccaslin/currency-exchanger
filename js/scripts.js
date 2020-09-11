2109559a391dbbe5c03f2e4d37e607ff61e5b1b3
David McCaslin <davidmccaslin94@gmail.com>
2020-09-11 14:33:35 -0700

    add additional app logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
+// Business Logic
+
+function convert(amount,currency) {
+  return amount * 2;
+}
+
+// UI Logic
+
 $(document).ready(function() {
   $("#formMain").submit(function(event) {
     event.preventDefault();
     const amount = parseInt($("input#amount").val());
-    console.log(amount);
+    //console.log(amount);
+    const convertedAmount = convert(amount);
+    $("#result").text(convertedAmount);
+    $("#results-group").show();
   })
 })
