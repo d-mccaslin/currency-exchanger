8202cccaeb44518aaf7730df24a33eeaec26a5ca
David McCaslin <davidmccaslin94@gmail.com>
2020-10-01 09:05:21 -0700

    update API call logic to incorporate environment key

--- src/main.js
+++ src/main.js
@@ -4,23 +4,14 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$("#formMain").submit(function(event) {
-  event.preventDefault();
-  const amount = parseInt($("input#amount").val());
-  const currency = $("select#currency").val();
-  //console.log(amount);
-  const convertedAmount = convert(amount,currency);
-  console.log(convertedAmount);
-  $("#result").text(convertedAmount);
-  $("#results-group").show();
-})
-
 $('#weatherLocation').click(function() {
   const city = $('#location').val();
   $('#location').val("");
 
+  const key = process.env.API_KEY;
+
   let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid={process.env.API_KEY}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
 
   request.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
@@ -32,9 +23,20 @@ $('#weatherLocation').click(function() {
   request.open("GET", url, true);
   request.send();
 
- function getElements(response) {
+  function getElements(response) {
     $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
   }
 });
+
+$("#formMain").submit(function(event) {
+  event.preventDefault();
+  const amount = parseInt($("input#amount").val());
+  const currency = $("select#currency").val();
+  //console.log(amount);
+  const convertedAmount = convert(amount,currency);
+  console.log(convertedAmount);
+  $("#result").text(convertedAmount);
+  $("#results-group").show();
+})
 
