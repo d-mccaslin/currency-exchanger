00217290a9362455b6070be57b91f878a4e9fef1
David McCaslin <davidmccaslin94@gmail.com>
2020-10-01 14:03:50 -0700

    update index and UI logic

--- src/main.js
+++ src/main.js
@@ -14,11 +14,17 @@ function clearFields() {
 function getElements(response,amount,target) {
   if (response.result == "success") {
     const rate = response.conversion_rates[target];
-    const convertedAmount = amount * rate;
-    $("#results-group").show();
-    $('.showRate').text(`The converted amount from USD to ${target} is ${convertedAmount}`);
+    if (rate) {
+      let convertedAmount = amount * rate;
+      convertedAmount = convertedAmount.toFixed(2);
+      $('#results-group').show();
+      $('.showRate').text(`The converted amount from USD to ${target} is ${convertedAmount}.`);
+    } else {
+      $('#results-group').show();
+      $('.showRate').text(`${target} is not a supported currency code. Please input a new target currency.`);
+    }
   } else {
-    $("results-group").show();
+    $('results-group').show();
     $('.showErrors').text(`There was an error: ${response}`);
   }
 }
