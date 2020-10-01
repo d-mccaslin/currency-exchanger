0b49d8c835e6dbc653493047f7f029ea15229966
David McCaslin <davidmccaslin94@gmail.com>
2020-10-01 12:10:54 -0700

    refactor exchange service logic, test new endpoint

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -2,8 +2,13 @@ export default class ExchangeService {
   static async getExchange(amount,target) {
     try {
       const key = process.env.API_KEY;
+      const amountParam = amount;
+      //const baseParam = 'USD';
+      //const targetParam = target;
       console.log(target);
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${key}/latest/${amount}`);
+
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${key}/latest/${amountParam}`);
+      //const response = await fetch(`https://pro.exchangerate-api.com/v6/${key}/pair/${baseParam}}/${targetParam}`);
       if (!response.ok) {
         throw Error(response.statusText);
       }
