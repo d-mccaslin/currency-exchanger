ed76840a81c420312f1dd2d780fb1e6a30667431
David McCaslin <davidmccaslin94@gmail.com>
2020-10-01 08:54:58 -0700

    set up env file and config

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ $('#weatherLocation').click(function() {
   $('#location').val("");
 
   let request = new XMLHttpRequest();
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[YOUR-API-KEY-HERE]`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid={process.env.API_KEY}`;
 
   request.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
